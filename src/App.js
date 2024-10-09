20b311667bdb0fe6a85a6bbcf245c7cd4db197da
刘皓譞 <liuhaoxuan@xiaopeizaiwandiannao.local>
2024-10-09 09:29:46 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+
+const text = 1111;
+const TextP = () => <p>{text}</p>;
+const Marker = ({ props1 }) => (
+  <div>
+    <p>{props1}</p>
+  </div>
+);
+const Map = ({ props1, props2 = 100 }) => (
+  <div>
+    <Marker props1={props1}></Marker>
+    <p>{props2}</p>
+  </div>
+);
+export default function Gallery() {
+  const [fs, setFs] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <TextP />
+      <Map props1={fs} />
+      <li className="item">{isPacked ? 1 + " ✅" : 1}</li>
+    </section>
   );
 }
-
-export default App;
